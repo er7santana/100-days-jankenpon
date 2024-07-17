45c34dbb1201ba6be41d78d05740f8dea60ae21f
Eliezer Sant Ana <eliezer.ana@f1rst.com.br>
2024-07-16 23:34:30 -0300

    Initial Commit

---
+++ Jankenpon/JankenponApp.swift
@@ -0,0 +1,17 @@
+//
+//  JankenponApp.swift
+//  Jankenpon
+//
+//  Created by Eliezer Rodrigo Beltramin de Sant Ana on 16/07/24.
+//
+
+import SwiftUI
+
+@main
+struct JankenponApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
