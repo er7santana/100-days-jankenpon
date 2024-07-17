796b78b9a7307913b3ac7fa2d67c86bdde686baf
Eliezer Sant Ana <eliezer.ana@f1rst.com.br>
2024-07-17 00:33:28 -0300

    Initial Commit

--- Jankenpon/ContentView.swift
+++ Jankenpon/ContentView.swift
@@ -7,15 +7,141 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+enum PlayOption {
+    case rock
+    case paper
+    case scissors
+    
+    var title: String {
+        switch self {
+        case .rock:
+            return "🪨"
+        case .paper:
+            return "🧻"
+        case .scissors:
+            return "✂️"
         }
-        .padding()
+    }
+    
+    var optionToWin: PlayOption {
+        switch self {
+        case .rock:
+            return .paper
+        case .paper:
+            return .scissors
+        case .scissors:
+            return .rock
+        }
+    }
+    
+    var optionToLose: PlayOption {
+        switch self {
+        case .rock:
+            return .scissors
+        case .paper:
+            return .rock
+        case .scissors:
+            return .paper
+        }
+    }
+}
+
+struct ContentView: View {
+    
+    let options = [PlayOption.paper, .rock, .scissors]
+    @State var currentOption = Int.random(in: 0..<3)
+    @State var shouldWin = Bool.random()
+    @State var currentScore = 0
+    @State var isShowingResultAlert = false
+    @State var alertTitle = ""
+    @State var questionsAnswered = 0
+    
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                Color.teal
+                    .opacity(0.4)
+                    .ignoresSafeArea()
+                
+                VStack {
+                    Spacer()
+                    Text("AI selected:")
+                        .font(.callout)
+                    Text(options[currentOption].title)
+                        .font(.system(size: 100))
+                        .fontWeight(.semibold)
+                        .padding(.bottom, 24)
+                    
+                    Text("You should:")
+                        .font(.callout)
+                    Text(shouldWin ? "Win" : "Lose")
+                        .font(.largeTitle)
+                        .fontWeight(.semibold)
+                        .padding(.bottom, 24)
+                    
+                    Text("Your choice:")
+                        .font(.callout)
+                    HStack {
+                        ForEach(options, id: \.self) { option in
+                            Button(action: {
+                                selectOption(option: option)
+                            }, label: {
+                                Text(option.title)
+                                    .font(.system(size: 100))
+                                    .foregroundStyle(.white)
+                                    .clipShape(RoundedRectangle(cornerRadius: 6))
+                            })
+                        }
+                    }
+                    
+                    Spacer()
+                    
+                    Text("Score: \(currentScore)")
+                        .font(.largeTitle)
+                        .fontWeight(.semibold)
+                    
+                    Spacer()
+                    
+                }
+                .padding()
+                .navigationTitle("Jankenpon")
+                .alert("Game over", isPresented: $isShowingResultAlert) {
+                    Button("Restart", action: restartGame)
+                } message: {
+                    Text("Final score: \(currentScore)")
+                        .font(.title)
+                }
+            }
+        }
+    }
+    
+    func restartGame() {
+        questionsAnswered = 0
+        currentScore = 0
+        nextOption()
+    }
+    
+    func nextOption() {
+        currentOption = Int.random(in: 0..<3)
+        shouldWin.toggle()
+    }
+    
+    func selectOption(option: PlayOption) {
+        questionsAnswered += 1
+        
+        let currentOption = options[currentOption]
+        if shouldWin && option == currentOption.optionToWin ||
+            !shouldWin && option == currentOption.optionToLose {
+            currentScore += 1
+        } else {
+            currentScore -= 1
+        }
+        
+        if questionsAnswered < 10 {
+            nextOption()
+        } else {
+            isShowingResultAlert = true
+        }
     }
 }
 
